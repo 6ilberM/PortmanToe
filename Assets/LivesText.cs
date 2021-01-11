771a83a1f5972410974c06537b61cda3a428fee1
Gilber Moreno <6ilberM@gmail.com>
2021-01-10 22:36:23 -0500

    Reset Some missing stuff

---
+++ Assets/LivesText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LivesText : MonoBehaviour
+{
+    int oldCharge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldCharge = GameManager.Instance.lives;
+        this.GetComponent<TMP_Text>().text = "Lives x " + GameManager.Instance.lives;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.lives != oldCharge)
+        {
+            UpdateText();
+            oldCharge = GameManager.Instance.lives;
+        }
+    }
+    void UpdateText()
+    {
+        this.GetComponent<TMP_Text>().text = "Lives x " + GameManager.Instance.lives;
+    }
+}
